d200afea20b9a47fd1008a216a8c0b357343e32d
Kiranpreet kaur <136697603+Kkkiiiirran@users.noreply.github.com>
2024-07-10 05:44:04 +0530

    updated port

--- main.py
+++ main.py
@@ -51,4 +51,4 @@ async def predict(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=10000, reload=True)
